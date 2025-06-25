746d28212f33c708a9e163e7000d9a03ee06f1a7
pablob44 <717522pb@eur.nl>
2025-06-25 20:54:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ if page == "Analytics":
     for _, row in top_modules.iterrows():
         st.markdown(f"- {row['title']}")
 elif page == "Modules":
-        st.title("Your Learning Modules")
+    st.title("Your Learning Modules")
 
     # --- Data Prep ---
     modules_df["learning_path"] = modules_df["learning_path"].fillna("external")
